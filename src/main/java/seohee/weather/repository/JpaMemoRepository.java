fbc84511cf0980d3f05c9b1b43ee6a12e9c230dd
moon-three <moondal366@gmail.com>
2025-10-03 02:00:32 +0900

    refactor: 사용하지 않는 클래스 삭제

--- src/main/java/seohee/weather/repository/JpaMemoRepository.java
+++
@@ -1,9 +0,0 @@
-package seohee.weather.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import seohee.weather.domain.Memo;
-
-@Repository
-public interface JpaMemoRepository extends JpaRepository<Memo, Integer> {
-}
